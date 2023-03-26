205596aa2d861fcf9fefe2fa952ae9021796f557
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-03-26 11:49:56 -0400

    javascript errors fixed

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const cityName = prompt("Enter the name of the city")
-cityName = cityName.replace(/\s+/g, '+');
-const apiUrl = `https://wttr.in/${cityName}?format=j1`
+const cityInput = document.getElementById("city-input")
+const weatherButton = document.querySelector(".upper-box-item")
+const previousSearches = document.querySelector(".previous-searches")
+
+weatherButton.addEventListener("click", () => {
+    const cityName = cityInput.replace(/\s+/g, "+");
+    const apiUrl = `https://wttr.in/${cityName}?format=j1`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const currentWeather = data.current_condition[0].temp_C;
+            const weatherBox = document.querySelector(".Weather-Box");
+            weatherBox.textContent = `Current weather in ${data.request.query}: ${currentWeather}°C`;
+
+            const previousSearchesList = previousSearches.querySelector("ul");
+            const newSearch = document.createElement("li");
+            newSearch.textContent = data.request.query;
+            previousSearchesList.appendChild(newSearch);
+        })
+        .catch(error =>{
+            console.error(error);
+        });
+
+});
